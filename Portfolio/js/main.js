17c4be869aa0b361aad304187b868844479bc9e0
booobooka <63097581+booobooka@users.noreply.github.com>
2020-05-13 13:01:07 +0300

    Portfolio

---
+++ Portfolio/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+      $('.slider__inner').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 7000,
+          });
+});
