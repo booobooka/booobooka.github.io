1cd5ae02a74c928315de8ba45435df0806cd6a28
booobooka <63097581+booobooka@users.noreply.github.com>
2020-05-15 19:29:08 +0300

    Add files via upload

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,41 +1,56 @@
-$(function() {
+$(function () {
 
-      wow = new WOW(
-            {
-            boxClass:     'wow',      // default
-            animateClass: 'animated', // default
-            offset:       0,          // default
-            mobile:       true,       // default
-            live:         true        // default
-          }
-          )
-          wow.init();
+  wow = new WOW(
+    {
+      boxClass: 'wow',      // default
+      animateClass: 'animated', // default
+      offset: 0,          // default
+      mobile: true,       // default
+      live: true        // default
+    }
+  )
+  wow.init();
 
-      $('.slider__inner').slick({
-            dots: true,
-            infinite: true,
-            speed: 500,
-            fade: true,
-            cssEase: 'linear',
-            arrows: false,
-            autoplay: true,
-            autoplaySpeed: 7000,
-          });
+  $('.slider__inner').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 7000,
+  });
 
-          let wrapperMenu = document.querySelector('.wrapper-menu');
-          wrapperMenu.addEventListener('click', function(){
-            wrapperMenu.classList.toggle('open');  
-          });
-          $('.wrapper-menu').on('click', function(){
-            $('.header__menu').slideToggle();
-          });
+  let wrapperMenu = document.querySelector('.wrapper-menu');
+  wrapperMenu.addEventListener('click', function () {
+    wrapperMenu.classList.toggle('open');
+  });
+  $('.wrapper-menu').on('click', function () {
+    $('.header__menu').slideToggle();
+  });
 
 
-          window.onload = function () {
-            document.body.classList.add('loaded_hiding');
-            window.setTimeout(function () {
-              document.body.classList.add('loaded');
-              document.body.classList.remove('loaded_hiding');
-            }, 500);
-          };
+  $('a[href*="#"]').on('click.smoothscroll', function (e) {
+    var hash = this.hash, _hash = hash.replace(/#/, ''), theHref = $(this).attr('href').replace(/#.*/, '');
+    if (theHref && location.href.replace(/#.*/, '') != theHref) return;
+    var $target = _hash === '' ? $('body') : $(hash + ', a[name="' + _hash + '"]').first();
+    if (!$target.length) return;
+    e.preventDefault();
+    $('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 600, 'swing', function () {
+      window.location.hash = hash;
+    });
+  });
+
+  $(window).on('load', function () {
+    $('body').addClass('loaded_hiding');
+    window.setTimeout(function () {
+      $('body').addClass('loaded');
+      $('body').removeClass('loaded_hiding');
+    }, 500);
+  });
+
+
+  
 });
+
